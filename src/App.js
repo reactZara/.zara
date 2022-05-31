f684932a3c4cabd4cbea24f2af143324e7f5e66f
parkdongmin <sayg1o1d@gmail.com>
2022-05-31 21:04:07 +0900

    Organisms folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg'
-import './App.css'
+import './App.css';
+import React from 'react';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
+                <img src="" className="App-logo" alt="logo" />
                 <a
                     className="App-link"
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+                    Learn Rea ctdddddddddsfsfddd
                 </a>
             </header>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
